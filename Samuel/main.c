0ec0380a025c26decab65ff8aa393f9e259c5ae8
Samuel Goubeau <samuel.goubeau@etu.univ-poitiers.fr>
2020-02-23 22:39:35 +0100

    ui

--- Samuel/main.c
+++ Samuel/main.c
@@ -5,13 +5,13 @@
 
 int main()
 {
-    int stop = 0;
-    char c;
+    int stop;
 
-    afficheMenu();                                 //Affiche le menu du jeu
+    afficheMenu();                                     //Affiche le menu du jeu
 
     while(stop == 0)
     {
+        stop = 0;                                      //Permet d'éviter un bug lorsqu'on relance une partie et qu'elle se finie
         system("cls");
         T_jeu *jeuN1 = initJeux(1, 10);                //Creer le niveau
 
